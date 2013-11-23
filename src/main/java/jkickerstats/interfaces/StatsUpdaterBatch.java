36b284b0b72fd0c6f899bf6b40b831a2e861de1b
joscha <zippelman@web.de>
2013-11-23 12:31:11 +0100

    scheduling updater batch works

--- src/main/java/jkickerstats/interfaces/StatsUpdaterBatch.java
+++ src/main/java/jkickerstats/interfaces/StatsUpdaterBatch.java
@@ -1,12 +1,13 @@
 package jkickerstats.interfaces;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 
-
-@Service
+@Component
+@EnableScheduling
 public class StatsUpdaterBatch {
 	@Autowired
 	private StatsUpdater statsUpdater;
