85c55275a4df600a349edc075d0ef5ce15ad80c0
joscha <joscha@harpeng.org>
2020-06-13 16:47:10 +0200

    remove mongodb and file backend

--- src/main/java/jkickerstats/Application.java
+++ src/main/java/jkickerstats/Application.java
@@ -1,16 +1,8 @@
 package jkickerstats;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.convert.DbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -18,23 +10,6 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
-
-    @Autowired
-    private MongoDbFactory mongoDbFactory;
-
-    @Autowired
-    private MongoMappingContext mongoMappingContext;
-
-    @Bean
-    public MappingMongoConverter mappingMongoConverter() {
-
-        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
-        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
-        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
-
-        return converter;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
